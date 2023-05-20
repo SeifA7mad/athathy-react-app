7cbc58386b20e12a986bc54d144155ad29f87ab4
Ahmad Sorour <ahmad_sorour@outlook.com>
2023-05-20 22:10:52 +0300

    feat: subcategories button redirect feature

--- src/components/page-content/products/listing/Subcategories.tsx
+++ src/components/page-content/products/listing/Subcategories.tsx
@@ -1,5 +1,9 @@
+import { APP_PREFIX_PATH } from '@src/configs/AppConfig';
+import { RouteKeysEnum } from '@src/configs/RoutesConfig';
+import useQueryParams from '@src/hooks/useQueryParams';
 import { CategoryType } from '@src/types/API/ProductType';
 import { Spin } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
   subcategories?: CategoryType[];
@@ -7,6 +11,12 @@ interface Props {
 }
 
 export default function Subcategories(props: Props) {
+  const navigate = useNavigate();
+
+  const onClick = (id: string, name: string) => {
+    navigate(`${APP_PREFIX_PATH}/${RouteKeysEnum.products}/${name}/${id}`);
+  };
+
   if (props.isFetching) {
     return <Spin className='!min-h-[18rem]' />;
   }
@@ -14,12 +24,13 @@ export default function Subcategories(props: Props) {
   return (
     <div className='w-full flex flex-wrap items-center gap-2'>
       {props.subcategories?.map((subcategory) => (
-        <div
+        <button
           key={subcategory.id}
           className='flex items-center justify-center text-center text-OuterSpace gap-2 w-48 h-14 rounded-md bg-white'
+          onClick={() => onClick(subcategory.id, subcategory.name)}
         >
           <span>{subcategory.name}</span>
-        </div>
+        </button>
       ))}
     </div>
   );
