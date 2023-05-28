7dccb3357ec2e6a655bd59dd26c9c5fa292aea08
Ahmad Sorour <ahmad_sorour@outlook.com>
2023-05-28 21:54:00 +0300

    feat: added another section to mobile landing

--- src/pages/mobile-landing.tsx/MobileLanding.tsx
+++ src/pages/mobile-landing.tsx/MobileLanding.tsx
@@ -1,13 +1,13 @@
 import playstore from 'src/assets/images/playStore.png';
 import appStore from 'src/assets/images/appStore.png';
 import img1 from 'src/assets/images/mobile-landing/1.png';
+import img2 from 'src/assets/images/mobile-landing/2.png';
 
 import './index.css';
-import LeftArrowSvg from '@src/assets/svg/LeftArrowSvg';
-import RightArrowSvg from '@src/assets/svg/RightArrowSvg';
 import { useRef, useState } from 'react';
 import LeftArrowCompleteSvg from '@src/assets/svg/LeftArrowCompleteSvg';
 import RightArrowCompleteSvg from '@src/assets/svg/RightArrowCompleteSvg';
+import Cart2Svg from '@src/assets/svg/Cart2Svg';
 
 export default function MobileLanding() {
   return (
@@ -55,65 +55,103 @@ export default function MobileLanding() {
             Get the app
           </button>
         </div>
-
-        <div className='flex flex-col gap-[26px] py-[32px] px-[23.5008px] bg-white rounded-[16px]'>
-          <div className='flex flex-col gap-[12px]'>
-            <h2 className='text-[#CDB6B2] text-sm font-extrabold'>
-              / ABOUT US
-            </h2>
-            <h3 className='text-[#212121] font-extrabold text-2xl leading-9'>
-              Your one-stop online furniture destination in Dubai.
-            </h3>
-          </div>
-          <img src={img1} />
-          <p className='text-OuterSpace leading-[24px] text-base'>
-            Serving to both customers and furniture brands around the U.A.E, our
-            mission is to make furniture shopping simple, convenient, and
-            accessible to everyone. Our e-commerce platform offers a wide range
-            of household furniture products from partnered brands, providing a
-            connection between these brands and a broader customer base.
-          </p>
-        </div>
-
-        <div className='flex flex-col gap-[26px] w-full'>
-          <div className='flex flex-col gap-[12px]'>
-            <h2 className='text-[#CDB6B2] text-sm font-extrabold'>
-              / OUR SERVICES
-            </h2>
-            <h3 className='text-[#212121] font-extrabold text-2xl leading-9'>
-              A tailored and personalized experience for our customers through
-              easy-to-use and unique features, making shopping for furniture a
-              breeze.
-            </h3>
-          </div>
-          <p className='text-OuterSpace leading-[24px] text-base'>
-            Our platform is designed to cater to all household furniture needs,
-            with a wide selection of brands and products all available under one
-            roof.
-          </p>
-          {/* Cards Carousel */}
-          <CardsCarousel
-            cards={[
-              {
-                title: 'Customizable Filters for Convenience',
-                description:
-                  'Enjoy a variety of filter options that they can adjust according to various factors such as brand, category, color, price, and much more.'
-              },
-              {
-                title: 'Effortless Product Visual Discovery',
-                description:
-                  'With our image search option, customers can easily find the same or similar product they are looking for with a click of a button.'
-              },
-              {
-                title: 'Expert Installation and Assembly',
-                description:
-                  'We provide installation/assembling services to make the process even smoother. '
-              }
-            ]}
-          />
-        </div>
+        <AboutUsSection />
+        <OurServicesSection />
+        <OurServicesForCustomersSection />
       </main>
     </>
+  );
+}
+
+function AboutUsSection() {
+  return (
+    <div className='flex flex-col gap-[26px] py-[32px] px-[23.5008px] bg-white rounded-[16px]'>
+      <div className='flex flex-col gap-[12px]'>
+        <h2 className='text-[#CDB6B2] text-sm font-extrabold'>/ ABOUT US</h2>
+        <h3 className='text-[#212121] font-extrabold text-2xl leading-9'>
+          Your one-stop online furniture destination in Dubai.
+        </h3>
+      </div>
+      <img src={img1} />
+      <p className='text-OuterSpace leading-[24px] text-base'>
+        Serving to both customers and furniture brands around the U.A.E, our
+        mission is to make furniture shopping simple, convenient, and accessible
+        to everyone. Our e-commerce platform offers a wide range of household
+        furniture products from partnered brands, providing a connection between
+        these brands and a broader customer base.
+      </p>
+    </div>
+  );
+}
+
+function OurServicesSection() {
+  return (
+    <section className='flex flex-col gap-[26px] w-full'>
+      <div className='flex flex-col gap-[12px]'>
+        <h2 className='text-[#CDB6B2] text-sm font-extrabold'>
+          / OUR SERVICES
+        </h2>
+        <h3 className='text-[#212121] font-extrabold text-2xl leading-9'>
+          A tailored and personalized experience for our customers through
+          easy-to-use and unique features, making shopping for furniture a
+          breeze.
+        </h3>
+      </div>
+      <p className='text-OuterSpace leading-[24px] text-base'>
+        Our platform is designed to cater to all household furniture needs, with
+        a wide selection of brands and products all available under one roof.
+      </p>
+      {/* Cards Carousel */}
+      <CardsCarousel
+        cards={[
+          {
+            title: 'Customizable Filters for Convenience',
+            description:
+              'Enjoy a variety of filter options that they can adjust according to various factors such as brand, category, color, price, and much more.'
+          },
+          {
+            title: 'Effortless Product Visual Discovery',
+            description:
+              'With our image search option, customers can easily find the same or similar product they are looking for with a click of a button.'
+          },
+          {
+            title: 'Expert Installation and Assembly',
+            description:
+              'We provide installation/assembling services to make the process even smoother. '
+          }
+        ]}
+      />
+    </section>
+  );
+}
+
+function OurServicesForCustomersSection() {
+  return (
+    <section className='flex flex-col gap-[26px] w-full'>
+      <div className='flex flex-col gap-[12px]'>
+        <h2 className='text-[#CDB6B2] text-sm font-extrabold'>
+          / OUR SERVICES FOR CUSTOMERS
+        </h2>
+        <h3 className='text-[#212121] font-extrabold text-2xl leading-9'>
+          Seamless and enjoyable shopping experience for our customers.
+        </h3>
+      </div>
+      <img src={img2} loading='lazy' />
+      <p className='text-OuterSpace leading-[24px] text-base'>
+        To achieve this, we have included a number of distinctive features on
+        both our website and app
+      </p>
+      <div className='flex flex-col bg-white rounded-[.75rem] py-[1.5rem] px-[.75rem] gap-[.875rem]'>
+        <span className='flex items-center justify-center w-[3.125rem] aspect-square rounded-full shadow-[0px_2px_6px_rgba(0,0,0,0.25)]'>
+          <Cart2Svg />
+        </span>
+        <p className='text-base font-medium'>
+          Wide selection of brands and products all available in one place,
+          customers can enjoy the convenience of shopping from the comfort of
+          their own homes.
+        </p>
+      </div>
+    </section>
   );
 }
 
