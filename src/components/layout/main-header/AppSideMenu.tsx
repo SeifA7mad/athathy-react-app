4488dcf4bda40755231da0dff1b56750978f91b6
Ahmad Sorour <ahmad_sorour@outlook.com>
2023-05-30 16:03:56 +0300

    fix: removed dropdown menu from profile icon

--- src/components/layout/main-header/AppSideMenu.tsx
+++ src/components/layout/main-header/AppSideMenu.tsx
@@ -5,15 +5,11 @@ import { APP_PREFIX_PATH } from '@src/configs/AppConfig';
 import { RouteKeysEnum } from '@src/configs/RoutesConfig';
 import { useAppSelector } from '@src/hooks/redux-hook';
 import useAuthModals from '@src/hooks/useAuthModals';
-import { Badge, Divider, Dropdown, MenuProps } from 'antd';
-import { Link, useNavigate } from 'react-router-dom';
-
-import { navigationItems } from '@src/components/page-content/dashboard/DashboardNavigation';
-import { auth } from '@src/configs/FirebaseConfig';
+import { Badge, Divider } from 'antd';
+import { useNavigate } from 'react-router-dom';
 import { QueriesKeysEnum } from '@src/configs/QueriesConfig';
 import { fetchCart } from '@src/services/CartService';
 import { useQuery } from '@tanstack/react-query';
-import { signOut } from 'firebase/auth';
 
 interface MenuItemProps {
   title?: string;
@@ -68,33 +64,6 @@ const AppSideMenu = (): JSX.Element => {
     navigate(`${APP_PREFIX_PATH}/${RouteKeysEnum.dashboard}`);
   };
 
-  const SideMenuItems: MenuProps['items'] = [
-    {
-      key: '1',
-      label: (
-        <button type='button' onClick={handleProfileClick}>
-          Dashboard
-        </button>
-      )
-    },
-    ...navigationItems.map((item) => ({
-      key: item.title,
-      label: (
-        <Link to={`${APP_PREFIX_PATH}/${RouteKeysEnum.dashboard}/${item.link}`}>
-          {item.title}
-        </Link>
-      )
-    })),
-    {
-      key: '2',
-      label: (
-        <button type='button' onClick={() => signOut(auth)}>
-          Sign out
-        </button>
-      )
-    }
-  ];
-
   return (
     <section className='flex items-center ml-auto gap-x-5 whitespace-nowrap'>
       {!isLoggedIn && (
@@ -105,15 +74,10 @@ const AppSideMenu = (): JSX.Element => {
         />
       )}
       {isLoggedIn && (
-        <>
-          <Dropdown
-            placement='bottom'
-            trigger={['click']}
-            menu={{ items: SideMenuItems }}
-          >
-            <MenuItem Icon={<ProfileSvg className='w-6 h-6' />} />
-          </Dropdown>
-        </>
+        <MenuItem
+          onClick={handleProfileClick}
+          Icon={<ProfileSvg className='w-6 h-6' />}
+        />
       )}
       <Divider type='vertical' className='border-turkishRose h-6' />
 
