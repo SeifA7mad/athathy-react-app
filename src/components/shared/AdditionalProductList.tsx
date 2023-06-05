159192843020611db9548f030c838e141b69357f
Ahmad Sorour <ahmad_sorour@outlook.com>
2023-06-05 23:51:14 +0300

    fix: how about these

--- src/components/shared/AdditionalProductList.tsx
+++ src/components/shared/AdditionalProductList.tsx
@@ -16,34 +16,34 @@ const AdditionalProductList = ({
   viewAllLink,
   products
 }: AdditionalProductListProps) => {
-  let responsive = [
-    {
-      breakpoint: 1536,
-      settings: {
-        slidesToShow: products.length > 5 ? 5 : products.length,
-        autoplay: products.length > 5
-      }
-    },
-    {
-      breakpoint: 1129,
-      settings: {
-        slidesToShow: products.length > 4 ? 4 : products.length,
-        autoplay: products.length > 4
-      }
-    },
-    {
-      breakpoint: 864,
-      settings: {
-        slidesToShow: products.length > 3 ? 3 : products.length
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: products.length > 2 ? 2 : products.length
-      }
-    }
-  ];
+  // let responsive = [
+  //   {
+  //     breakpoint: 1536,
+  //     settings: {
+  //       slidesToShow: products.length > 5 ? 5 : products.length,
+  //       autoplay: products.length > 5
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 1129,
+  //     settings: {
+  //       slidesToShow: products.length > 4 ? 4 : products.length,
+  //       autoplay: products.length > 4
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 864,
+  //     settings: {
+  //       slidesToShow: products.length > 3 ? 3 : products.length
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       slidesToShow: products.length > 2 ? 2 : products.length
+  //     }
+  //   }
+  // ];
 
   return (
     <div className='w-full max-w-[60rem] lg:max-w-[76rem] 2xl:max-w-[90rem] flex flex-col gap-y-10'>
@@ -66,12 +66,13 @@ const AdditionalProductList = ({
         autoplay={products.length > 6}
         slidesToShow={products.length > 6 ? 6 : products.length}
         centerMode={false}
-        infinite={true}
+        infinite={false}
         dots={false}
-        responsive={responsive}
+        prefixCls='ant-additional-product-list'
+        // responsive={responsive}
       >
         {products?.map((product, index) => (
-          <ProductCard className='' key={product.id + index} {...product} />
+          <ProductCard key={product.id + index} {...product} />
         ))}
       </Carousel>
     </div>
