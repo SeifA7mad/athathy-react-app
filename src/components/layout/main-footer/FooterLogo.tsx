e44b2cc4ed358cb788fae89273bc1cfae95bba44
Ahmad Sorour <ahmad_sorour@outlook.com>
2023-06-13 10:20:50 +0300

    design fix: footer logo

--- src/components/layout/main-footer/FooterLogo.tsx
+++ src/components/layout/main-footer/FooterLogo.tsx
@@ -2,8 +2,13 @@ import LogoImage from '@src/assets/images/logos/LOGO2.png';
 
 const FooterLogo = (): JSX.Element => {
   return (
-    <div className='flex flex-col place-items-center gap-y-5'>
-      <img className='' src={LogoImage} alt='Logo' loading='lazy' />
+    <div className='flex flex-col place-items-center gap-y-[1.2769rem]'>
+      <img
+        className='w-[6.25rem] h-[4.2231rem]'
+        src={LogoImage}
+        alt='Logo'
+        loading='lazy'
+      />
       <p className='text-white text-2xl font-bold'>ATHATHY</p>
     </div>
   );
